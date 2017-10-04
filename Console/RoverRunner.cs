1208424f03dad02b8f57c36dea401beb607eed71
Joshua Lewis <joshilewis@gmail.com>
2017-10-05 00:21:35 +0200

    refactor: restructures code to improve expressability

--- Console/RoverRunner.cs
+++ Console/RoverRunner.cs
@@ -24,6 +24,12 @@ namespace Console
         };
 
         private readonly string inputFile;
+        private int maxX;
+        private int maxY;
+        private int currentX;
+        private int currentY;
+        private string currentDirection;
+        private string commands;
 
         public RoverRunner(string inputFile)
         {
@@ -32,64 +38,84 @@ namespace Console
 
         public string Run()
         {
+            Initialise();
+
+            foreach (char command in commands)
+            {
+                ProcessCommand(command);
+            }
+
+            return $"{currentX} {currentY} {currentDirection}";
+        }
+
+        private void ProcessCommand(char command)
+        {
+            switch (command)
+            {
+                default: throw new ArgumentException("Invalid command: " + command);
+                case 'R':
+                    currentDirection = rMapping[currentDirection];
+                    break;
+                case 'L':
+                    currentDirection = lMapping[currentDirection];
+                    break;
+                case 'M':
+                    MoveRover();
+
+                    break;
+            }
+        }
+
+        private void MoveRover()
+        {
+            switch (currentDirection)
+            {
+                case "N":
+                    if (currentY == maxY - 1) throw new RoverOutOfBoundsException("North");
+                    currentY++;
+                    break;
+                case "E":
+                    if (currentX == maxX - 1) throw new RoverOutOfBoundsException("East");
+                    currentX++;
+                    break;
+                case "S":
+                    if (currentY == 0) throw new RoverOutOfBoundsException("South");
+                    currentY--;
+                    break;
+                case "W":
+                    if (currentX == 0) throw new RoverOutOfBoundsException("West");
+                    currentX--;
+                    break;
+            }
+        }
+
+        private void Initialise()
+        {
             string[] fileContents = File.ReadAllLines(inputFile);
 
             string[] zoneSizeContents = fileContents[0]
                 .Split(' ')
                 .ToArray();
-            int maxX = int.Parse(zoneSizeContents[0]);
-            int maxY = int.Parse(zoneSizeContents[1]);
+            maxX = int.Parse(zoneSizeContents[0]);
+            maxY = int.Parse(zoneSizeContents[1]);
 
             string[] currentStateContents = fileContents[1]
                 .Split(' ')
                 .ToArray();
 
-            int currentX = int.Parse(currentStateContents[0]);
+            currentX = int.Parse(currentStateContents[0]);
             if (currentX < 0) throw new ArgumentException("Negative starting X");
             if (currentX == maxX) throw new RoverOutOfBoundsException("East");
-            int currentY = int.Parse(currentStateContents[1]);
+
+            currentY = int.Parse(currentStateContents[1]);
             if (currentY < 0) throw new ArgumentException("Negative starting Y");
             if (currentY == maxY) throw new RoverOutOfBoundsException("North");
-            string currentDirection = currentStateContents[2];
-            if (!new[] { "N", "E", "S", "W" }.Contains(currentDirection))
+
+            currentDirection = currentStateContents[2];
+            if (!new[] {"N", "E", "S", "W"}.Contains(currentDirection))
                 throw new ArgumentException("Invalid starting direction: " + currentDirection);
-            foreach (char command in fileContents[2])
-            {
-                switch (command)
-                {
-                    default: throw new ArgumentException("Invalid command: " + command);
-                    case 'R':
-                        currentDirection = rMapping[currentDirection];
-                        break;
-                    case 'L':
-                        currentDirection = lMapping[currentDirection];
-                        break;
-                    case 'M':
-                        switch (currentDirection)
-                        {
-                            case "N":
-                                if (currentY == maxY - 1) throw new RoverOutOfBoundsException("North");
-                                currentY++;
-                                break;
-                            case "E":
-                                if (currentX == maxX - 1) throw new RoverOutOfBoundsException("East");
-                                currentX++;
-                                break;
-                            case "S":
-                                if (currentY == 0) throw new RoverOutOfBoundsException("South");
-                                currentY--;
-                                break;
-                            case "W":
-                                if (currentX == 0) throw new RoverOutOfBoundsException("West");
-                                currentX--;
-                                break;
-                        }
 
-                        break;
-                }
-            }
-
-            return $"{currentX} {currentY} {currentDirection}";
+            commands = fileContents[2];
         }
     }
 }

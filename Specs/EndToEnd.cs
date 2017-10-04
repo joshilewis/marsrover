fbd9db6d17aa8dc064446a53636eda47b9e3bf69
Joshua Lewis <joshilewis@gmail.com>
2017-10-04 21:47:28 +0200

    adds first unsatisfied spec - Real World Scenario 1

---
+++ Specs/EndToEnd.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Specs
+{
+    [TestFixture]
+    public class EndToEnd
+    {
+        [Test]
+        public void RealWorld1()
+        {
+            var inputFile = "RealWorld1.txt";
+            var expected = "3 3 S";
+            Assert.That(RunRover(inputFile), Is.EqualTo(expected));
+        }
+    }
+}
